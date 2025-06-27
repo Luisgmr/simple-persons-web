40d193e03ece33e024d49169a40191e8e0ca3fa7
Luis Gustavo Miranda <luisgmr.privado@hotmail.com>
2025-06-27 19:55:59 -0300

    Implementado ambiente de desenvolvimento e containers docker

---
+++ backend/src/test/java/com/luisgmr/senai/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisgmr.senai.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
